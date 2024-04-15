fd39d84e3ec3e199791ee32ec8c4ddd1c1d21305
Pablo Mendoza <asdson5555@gmail.com>
2024-04-14 20:11:30 -0500

    ServiciosArray en la interface

--- TareaTanaka04GestiónDePaises/Juego.cs
+++ TareaTanaka04GestiónDePaises/Juego.cs
@@ -10,7 +10,10 @@ namespace TareaTanaka04GestiónDePaises
     {
         public void Execute()
         {
-            
+            Servicios[] serviciosArray = new Servicios[3];
+            serviciosArray[0] = new Salud("Salud", 0,0 );
+            serviciosArray[1] = new Educación("Educacion", 0,0 );
+            serviciosArray[2] = new Seguridad("Seguridad", 0,0 );
         }
         
     }
