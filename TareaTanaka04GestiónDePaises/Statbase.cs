39f2bb8cfc2408c3d3cc6e6b8acfa9d259651809
Ricardo Pariona Peralta <147557101+RickParionaChubascos@users.noreply.github.com>
2024-04-14 20:21:37 -0500

    StatBase

---
+++ TareaTanaka04GestiónDePaises/Statbase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaTanaka04GestiónDePaisesñ 
+{
+    internal class StatBase
+    {
+        public StatBase()
+        {
+            protected string name { get;: set };
+        }
+        public virtual float GetPopulation() 
+        {
+            return 0;
+        }
+        public virtual float GetMoney()
+        {
+            return 0;
+        }
+        public virtual float GetHappy()
+        {
+            return 0;
+        }
+    }
+}
